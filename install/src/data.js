94b8cf1841ba6d1ef54ef07343e6118b14f3f527
Jack <ahfhafh@gmail.com>
2021-08-29 17:57:12 -0600

    import bootstrap

---
+++ install/src/data.js
@@ -0,0 +1,15 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'cat food',
+            category: 'foods',
+            image:'/images/foods.jpg',
+            price: 42,
+            brand: 'Yes',
+            rating: 4.5,
+            numReviews: 14,
+            description: 'high quality cat food',
+        }
+    ]
+}
